34c00a809577c7433a4d68bbd79dcf8728d48b18
Mo Amini <mo.amini@nav.no>
2022-02-17 10:35:50 +0100

    Fjerner ubrukt metode og lib

--- src/main/kotlin/no/nav/eessi/pensjon/models/PersonModel.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/models/PersonModel.kt
@@ -1,8 +1,6 @@
 package no.nav.eessi.pensjon.models
 
 import no.nav.eessi.pensjon.eux.model.SedType
-import no.nav.eessi.pensjon.personoppslag.Fodselsnummer
-import no.nav.eessi.pensjon.personoppslag.pdl.model.SokKriterier
 import java.time.LocalDate
 
 data class IdentifisertPerson(
@@ -21,24 +19,13 @@ data class IdentifisertPerson(
 }
 
 data class SEDPersonRelasjon(
-    val fnr: Fodselsnummer?,
+    val fnr: String?,
     val relasjon: Relasjon,
     val saktype: Saktype? = null,
     val sedType: SedType? = null,
-    val sokKriterier: SokKriterier? = null,
     val fdato: LocalDate? = null,
     val rinaDocumentId: String
-) {
-    fun isFnrDnrSinFdatoLikSedFdato(): Boolean {
-        //fdato == null and return true validation allow fnr
-        //fdato == null and return false validation fail
-        if (fdato == null) return true
-
-        return fnr?.getBirthDate() == fdato
-    }
-
-
-}
+)
 
 enum class Relasjon {
     FORSIKRET,
