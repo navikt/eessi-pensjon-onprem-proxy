af60ad698bdc8b1ddc2b35a82aff7158fe5fee86
Daniel Skarpås <dskarpas@gmail.com>
2022-02-24 12:35:58 +0100

    Fjerner Norg2 service og modeller og forenkler uthenting uten json formatering
    Co-authored-by: Jens Christian Madsen <jens.christian.madsen@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/api/Norg2Api.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/Norg2Api.kt
@@ -1,7 +1,6 @@
 package no.nav.eessi.pensjon.api
 
-import no.nav.eessi.pensjon.klienter.norg2.Norg2Service
-import no.nav.eessi.pensjon.klienter.norg2.NorgKlientRequest
+import no.nav.eessi.pensjon.klienter.norg2.Norg2Klient
 import no.nav.eessi.pensjon.kodeverk.KodeverkClient
 import no.nav.eessi.pensjon.models.Enhet
 import no.nav.security.token.support.core.api.Protected
@@ -9,12 +8,12 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @Protected
-class ProxyController(private val norg2Service: Norg2Service,
+class ProxyController(private val norg2Service: Norg2Klient,
                       private val kodeverkClient: KodeverkClient) {
 
     @PostMapping("/api/v1/arbeidsfordeling")
-    fun norgArbeidsfordeling(@RequestBody req : NorgKlientRequest): Enhet? {
-        return norg2Service.hentArbeidsfordelingEnhet(req)
+    fun norgArbeidsfordeling(@RequestBody req : String): String? {
+        return norg2Service.hentArbeidsfordelingEnheter(req)
     }
 
     @GetMapping("/api/v1/hierarki/{hierarki}/noder")
