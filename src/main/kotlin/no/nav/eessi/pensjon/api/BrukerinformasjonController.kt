32b2eeb58eced7f5fce1dc513295712c1eab1e45
Daniel Skarpås <dskarpas@gmail.com>
2023-03-30 09:00:34 +0200

    r - Legger inn logging for henting av brukerident

--- src/main/kotlin/no/nav/eessi/pensjon/api/BrukerinformasjonController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/BrukerinformasjonController.kt
@@ -5,6 +5,8 @@ import no.nav.eessi.pensjon.ldap.BrukerInformasjon
 import no.nav.eessi.pensjon.ldap.BrukerInformasjonService
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import no.nav.security.token.support.core.api.Protected
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 class BrukerinformasjonController(
     private val ldapService: BrukerInformasjonService,
     @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()) {
+    private val logger: Logger by lazy { LoggerFactory.getLogger(BrukerinformasjonController::class.java) }
 
     private lateinit var proxyLDAP: MetricsHelper.Metric
 
@@ -25,6 +28,7 @@ class BrukerinformasjonController(
 
     @GetMapping("/brukerinfo/{navident}")
     fun hentBrukerInformasjon(@PathVariable("navident", required = true) navident: String): BrukerInformasjon {
+        logger.info("Henter brukerinformasjon for Ident: $navident")
         return proxyLDAP.measure {
             ldapService.hentBrukerInformasjon(navident)
         }
