0bf4085825bbee7432ebb81fa42620cb2aa389eb
Daniel Skarpås <dskarpas@gmail.com>
2023-04-13 10:35:51 +0200

    F - Sletter kodeverk fra proxy
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/api/ProxyController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/ProxyController.kt
@@ -2,7 +2,6 @@ package no.nav.eessi.pensjon.api
 
 import jakarta.annotation.PostConstruct
 import no.nav.eessi.pensjon.klienter.Norg2Klient
-import no.nav.eessi.pensjon.kodeverk.KodeverkClient
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,16 +10,13 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @Protected
 class ProxyController(private val norg2Klient: Norg2Klient,
-                      private val kodeverkClient: KodeverkClient,
                       @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()) {
 
     private lateinit var proxyNorg2: MetricsHelper.Metric
-    private lateinit var proxyKodeverk: MetricsHelper.Metric
 
     @PostConstruct
     fun initMetrics() {
         proxyNorg2 = metricsHelper.init("proxyNorg2")
-        proxyKodeverk = metricsHelper.init("proxyKodeverk")
     }
 
     @PostMapping("/api/v1/arbeidsfordeling")
@@ -29,12 +25,4 @@ class ProxyController(private val norg2Klient: Norg2Klient,
             norg2Klient.hentArbeidsfordelingEnheter(req)
         }
     }
-
-    @GetMapping("/api/v1/hierarki/{hierarki}/noder")
-    private fun kentLandkoderFraKodeverk(@PathVariable("hierarki", required = true) hierarki: String) : String {
-        return proxyKodeverk.measure {
-            kodeverkClient.hentHierarki(hierarki)
-        }
-    }
-
 }
