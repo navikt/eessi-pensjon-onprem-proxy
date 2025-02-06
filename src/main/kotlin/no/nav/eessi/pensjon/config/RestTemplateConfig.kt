64bfb6f4d3596f854db72c9c15fb4003f7f29100
mariampervez <mariam.pervez@nav.no>
2025-02-06 13:44:36 +0100

    F - Legger til restTemplate config for pen obo token

--- src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/RestTemplateConfig.kt
@@ -1,6 +1,10 @@
 package no.nav.eessi.pensjon.config
 
+import com.nimbusds.jwt.JWTClaimsSet
 import io.micrometer.core.instrument.MeterRegistry
+import no.nav.common.token_client.builder.AzureAdTokenClientBuilder
+import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient
+import no.nav.eessi.pensjon.errorhandler.PenErrorHandler
 import no.nav.eessi.pensjon.logging.RequestIdHeaderInterceptor
 import no.nav.eessi.pensjon.logging.RequestResponseLoggerInterceptor
 import no.nav.eessi.pensjon.metrics.RequestCountInterceptor
@@ -11,31 +15,40 @@ import no.nav.eessi.pensjon.shared.retry.IOExceptionRetryInterceptor
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.security.token.support.core.jwt.JwtToken
+import no.nav.security.token.support.core.jwt.JwtTokenClaims
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpRequest
-import org.springframework.http.client.BufferingClientHttpRequestFactory
-import org.springframework.http.client.ClientHttpRequestExecution
-import org.springframework.http.client.ClientHttpRequestInterceptor
-import org.springframework.http.client.ClientHttpResponse
-import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.http.client.*
 import org.springframework.http.client.support.BasicAuthenticationInterceptor
 import org.springframework.web.client.DefaultResponseErrorHandler
 import org.springframework.web.client.HttpStatusCodeException
+import org.springframework.web.client.ResponseErrorHandler
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 import java.time.Duration
+import java.util.*
 
 @Configuration
 class RestTemplateConfig(
     private val clientConfigurationProperties: ClientConfigurationProperties,
     private val oAuth2AccessTokenService: OAuth2AccessTokenService,
     private val securityTokenExchangeService: STSService,
+    private val tokenValidationContextHolder: TokenValidationContextHolder,
     private val meterRegistry: MeterRegistry
 ) {
+    private val logger = LoggerFactory.getLogger(RestTemplateConfig::class.java)
+    private val secureLog = LoggerFactory.getLogger("secureLog")
+
+    @Value("\${AZURE_APP_PEN_CLIENT_ID}")
+    lateinit var pensjonClientId: String
 
     @Value("\${PENSJONSINFORMASJON_URL}")
     lateinit var peninfourl: String
@@ -59,7 +72,7 @@ class RestTemplateConfig(
     fun bestemSakOidcRestTemplate() = buildRestTemplate(bestemSakUrl)
 
     @Bean
-    fun pensjonInformasjonRestTemplate() = restTemplate(peninfourl, CustomUsernameToOidcInterceptor(srvFagmodulUsername, srvFagmodulPassword, securityTokenExchangeService))
+    fun pensjonInformasjonRestTemplate() = opprettRestTemplate(peninfourl, onBehalfOfBearerTokenInterceptor(pensjonClientId), PenErrorHandler())
 
     @Bean
     fun behandleHendelseRestTemplate() = restTemplate(penBeandleHendelseurl, CustomUsernameToOidcInterceptor(srvFagmodulUsername, srvFagmodulPassword, securityTokenExchangeService))
@@ -70,6 +83,28 @@ class RestTemplateConfig(
             ?: throw RuntimeException("could not find oauth2 client config for ${"fagmodul-credentials"}"),
         oAuth2AccessTokenService
     ))
+
+    private fun onBehalfOfBearerTokenInterceptor(clientId: String): ClientHttpRequestInterceptor {
+        logger.info("init onBehalfOfBearerTokenInterceptor: $clientId")
+        return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
+            val decodedToken = URLDecoder.decode(getToken(tokenValidationContextHolder).encodedToken, StandardCharsets.UTF_8)
+
+            secureLog.info("NAVIdent: ${getClaims(tokenValidationContextHolder).get("NAVident")?.toString()}")
+
+            val tokenClient: AzureAdOnBehalfOfTokenClient = AzureAdTokenClientBuilder.builder()
+                .withNaisDefaults()
+                .buildOnBehalfOfTokenClient()
+
+            val accessToken: String = tokenClient.exchangeOnBehalfOfToken(
+                "api://$clientId/.default",
+                decodedToken
+            )
+            logger.debug("accessToken: $accessToken")
+
+            request.headers.setBearerAuth(accessToken)
+            execution.execute(request, body!!)
+        }
+    }
 
     private fun buildRestTemplate(url: String): RestTemplate {
         return RestTemplateBuilder()
@@ -105,6 +140,44 @@ class RestTemplateConfig(
             }
     }
 
+    private fun opprettRestTemplate(url: String, tokenInterceptor: ClientHttpRequestInterceptor?, defaultErrorHandler: ResponseErrorHandler = DefaultResponseErrorHandler()) : RestTemplate {
+        logger.info("init restTemplate: $url")
+        return RestTemplateBuilder()
+            .rootUri(url)
+            .errorHandler(defaultErrorHandler)
+            .readTimeout(Duration.ofSeconds(120))
+            .connectTimeout(Duration.ofSeconds(120))
+            .additionalInterceptors(
+                RequestIdHeaderInterceptor(),
+                IOExceptionRetryInterceptor(),
+                RequestCountInterceptor(meterRegistry),
+                RequestResponseLoggerInterceptor(),
+                tokenInterceptor
+            )
+            .build().apply {
+                requestFactory = BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory())
+            }
+    }
+
+    private fun clientProperties(oAuthKey: String): ClientProperties {
+        return Optional.ofNullable(clientConfigurationProperties.registration[oAuthKey])
+            .orElseThrow { RuntimeException("could not find oauth2 client config for example-onbehalfof") }
+    }
+
+    private fun bearerTokenInterceptor(
+        clientProperties: ClientProperties,
+        oAuth2AccessTokenService: OAuth2AccessTokenService
+    ): ClientHttpRequestInterceptor {
+        return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray?, execution: ClientHttpRequestExecution ->
+            val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+            val tokenChunks = response.access_token!!.split(".")
+            val tokenBody =  tokenChunks[1]
+            logger.debug("subject: " + JWTClaimsSet.parse(Base64.getDecoder().decode(tokenBody).decodeToString()).subject + "/n + $response.accessToken")
+
+            request.headers.setBearerAuth(response.access_token!!)
+            execution.execute(request, body!!)
+        }
+    }
     private fun oAuth2BearerTokenInterceptor(
         clientProperties: ClientProperties,
         oAuth2AccessTokenService: OAuth2AccessTokenService
@@ -165,5 +238,34 @@ class RestTemplateConfig(
                 ).build()
         }
     }
+
+    fun getClaims(tokenValidationContextHolder: TokenValidationContextHolder): JwtTokenClaims {
+        val context = tokenValidationContextHolder.getTokenValidationContext()
+        if(context.issuers.isEmpty())
+            throw RuntimeException("No issuer found in context")
+
+        val validIssuer = context.issuers.filterNot { issuer ->
+            val oidcClaims = context.getClaims(issuer)
+            oidcClaims.expirationTime.before(Date())
+        }.map { it }
+
+
+        if (validIssuer.isNotEmpty()) {
+            val issuer = validIssuer.first()
+            return context.getClaims(issuer)
+        }
+        throw RuntimeException("No valid issuer found in context")
+
+    }
+
+    fun getToken(tokenValidationContextHolder: TokenValidationContextHolder): JwtToken {
+        val context = tokenValidationContextHolder.getTokenValidationContext()
+        if(context.issuers.isEmpty())
+            throw RuntimeException("No issuer found in context")
+        val issuer = context.issuers.first()
+
+        return context.getJwtToken(issuer)!!
+    }
+
 }
 
---
+++ src/main/kotlin/no/nav/eessi/pensjon/errorhandler/PenErrorHandler.kt
@@ -0,0 +1,94 @@
+package no.nav.eessi.pensjon.errorhandler
+
+import no.nav.eessi.pensjon.eux.klient.*
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.*
+import org.springframework.http.client.ClientHttpResponse
+import org.springframework.util.StreamUtils
+import org.springframework.web.client.ResponseErrorHandler
+import org.springframework.web.server.ResponseStatusException
+import java.io.IOException
+import java.nio.charset.Charset
+
+class PenErrorHandler: ResponseErrorHandler {
+
+    /**
+     * PenErrorHandler er en utvidelse av DefaultResponseErrorHandler som håndterer
+     * HTTP-feil for PEN integrasjon.
+     *
+     * Den har som ansvar å:
+     *  - Sjekke om en HTTP-respons inneholder en feil (4xx/5xx statuskoder).
+     *  - Logge responsen, inkludert detaljert feilmelding og responsbody.
+     *  - Kaste spesifikke unntak (exceptions) basert på ulike HTTP-statuskoder, som f.eks.
+     *    500 (Internal Server Error), 404 (Not Found), 401 (Unauthorized) osv.
+     *  - Fange opp ukjente feil og logge disse for videre debugging.
+     */
+    private val logger = LoggerFactory.getLogger(PenErrorHandler::class.java)
+
+    @Throws(IOException::class)
+    override fun hasError(response: ClientHttpResponse): Boolean {
+        return response.statusCode.isError
+    }
+
+    @Throws(IOException::class)
+    override fun handleError(httpResponse: ClientHttpResponse) {
+        logResponse(httpResponse)
+
+        when (httpResponse.statusCode) {
+            BAD_REQUEST -> handleBadRequest(httpResponse)
+            NOT_FOUND -> throw IkkeFunnetException("Ikke funnet")
+            FORBIDDEN -> throw ForbiddenException("Forbidden, Ikke tilgang")
+            CONFLICT -> throw EuxConflictException("En konflikt oppstod under kall til Pensjon-pen")
+            UNAUTHORIZED -> throw RinaIkkeAutorisertBrukerException("Authorization token required.")
+            GATEWAY_TIMEOUT -> throw GatewayTimeoutException("Venting på respons fra pensjon-pen resulterte i en timeout")
+            INTERNAL_SERVER_ERROR -> throw EuxRinaServerException("pensjon-pen serverfeil, kan også skyldes ugyldig input")
+            else -> handleBadRequest(httpResponse)
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun handleBadRequest(httpResponse: ClientHttpResponse) {
+        val responseBody = StreamUtils.copyToString(httpResponse.body, Charset.defaultCharset())
+        if (responseBody.contains("postalCode")) {
+            throw KanIkkeOppretteSedFeilmelding("Postnummer overskrider maks antall tegn (25) i PDL.")
+        }
+        throw GenericUnprocessableEntity("Bad request, en feil har oppstått")
+    }
+
+    @Throws(IOException::class)
+    private fun logResponse(response: ClientHttpResponse) {
+        val errorMsg = StreamUtils.copyToString(response.body, Charset.defaultCharset())
+        val callingClass = getCallingClasses()
+        val logMessage = """
+            Calling class: ${callingClass}
+            Status code  : ${response.statusCode}
+            Response body: $errorMsg""".trimIndent()
+
+        if (errorMsg.contains("Could not find RINA case with id")) {
+            logger.warn(logMessage)
+        } else {
+            logger.error(logMessage)
+        }
+    }
+
+    private fun getCallingClasses(): List<String> {
+        val stackTrace = Thread.currentThread().stackTrace
+
+        val excludedClasses =
+            setOf(this::class.java.name, "ResponseErrorHandler", "RestTemplate", "MetricsHelper", "Metric")
+
+        return stackTrace.drop(2)
+            .take(15)
+            // filterer bort classer som ikke er relevant for logging
+            .filter { element ->
+                excludedClasses.none { excluded -> element.className.contains(excluded) } &&
+                        element.className.contains("eessi")  // Includes only results containing "eessi"
+            }
+            .map { element ->
+                "${element.className}.${element.methodName}"
+            }
+    }
+}
+
+open class KanIkkeOppretteSedFeilmelding(message: String?) : ResponseStatusException(HttpStatus.BAD_REQUEST, message)
