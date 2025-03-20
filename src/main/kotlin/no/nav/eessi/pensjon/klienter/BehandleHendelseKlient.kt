1fe99fdf85350a95ccb39a7f5fa72e770c8c9eb4
Daniel Skarpås <dskarpas@gmail.com>
2025-03-20 13:00:11 +0100

    Revert "r - faser ut alt av pensjon og andre API"
    This reverts commit e1a55e9a796606495492ebc4cb4ea03947a48935.

--- src/main/kotlin/no/nav/eessi/pensjon/klienter/BehandleHendelseKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/klienter/BehandleHendelseKlient.kt
@@ -10,32 +10,32 @@ import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.server.ResponseStatusException
 
-//@Component
-//class BehandleHendelseKlient(
-//    private val behandleHendelseRestTemplate: RestTemplate) {
-//
-//    private val logger = LoggerFactory.getLogger(BehandleHendelseKlient::class.java)
-//
-//    fun opprettBehandleHendelse(utlandsHendelsemodel: String) {
-//        try {
-//            val headers = HttpHeaders()
-//            headers.contentType = MediaType.APPLICATION_JSON
-//            val httpEntity = HttpEntity(utlandsHendelsemodel, headers)
-//            logger.debug("*** legger følgende melding på behandlehendlse tjenesten: $utlandsHendelsemodel ***")
-//
-//            behandleHendelseRestTemplate.postForEntity(
-//                "/",
-//                httpEntity,
-//                String::class.java
-//            )
-//        } catch (ex: HttpStatusCodeException) {
-//            logger.error("En feil oppstod under opprettelse av behandlehendlse ex: ", ex)
-//            throw ResponseStatusException(ex.statusCode, "En feil oppstod under opprettelse av behandlehendelse ex: ${ex.message} body: ${ex.responseBodyAsString}")
-//        } catch (ex: Exception) {
-//            logger.error("En feil oppstod under opprettelse av behandlehendlse ex: ", ex)
-//            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"En feil oppstod under opprettelse av behandlehendelse ex: ${ex.message}")
-//        }
-//
-//        logger.debug("*** Ferdig med melding ***")
-//    }
-//}
+@Component
+class BehandleHendelseKlient(
+    private val behandleHendelseRestTemplate: RestTemplate) {
+
+    private val logger = LoggerFactory.getLogger(BehandleHendelseKlient::class.java)
+
+    fun opprettBehandleHendelse(utlandsHendelsemodel: String) {
+        try {
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.APPLICATION_JSON
+            val httpEntity = HttpEntity(utlandsHendelsemodel, headers)
+            logger.debug("*** legger følgende melding på behandlehendlse tjenesten: $utlandsHendelsemodel ***")
+
+            behandleHendelseRestTemplate.postForEntity(
+                "/",
+                httpEntity,
+                String::class.java
+            )
+        } catch (ex: HttpStatusCodeException) {
+            logger.error("En feil oppstod under opprettelse av behandlehendlse ex: ", ex)
+            throw ResponseStatusException(ex.statusCode, "En feil oppstod under opprettelse av behandlehendelse ex: ${ex.message} body: ${ex.responseBodyAsString}")
+        } catch (ex: Exception) {
+            logger.error("En feil oppstod under opprettelse av behandlehendlse ex: ", ex)
+            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,"En feil oppstod under opprettelse av behandlehendelse ex: ${ex.message}")
+        }
+
+        logger.debug("*** Ferdig med melding ***")
+    }
+}
--- src/main/kotlin/no/nav/eessi/pensjon/klienter/BestemSakKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/klienter/BestemSakKlient.kt
@@ -19,54 +19,54 @@ import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 import java.util.*
 
-//@Component
-//class BestemSakKlient(private val bestemSakOidcRestTemplate: RestTemplate,
-//                      @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()) {
-//
-//    private val logger: Logger by lazy { LoggerFactory.getLogger(BestemSakKlient::class.java) }
-//    private val mapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
-//
-//    private lateinit var hentPesysSaker: MetricsHelper.Metric
-//
-//    init {
-//        hentPesysSaker = metricsHelper.init("hentPesysSaker")
-//    }
-//
-//    /**
-//     * Henter pesys sakID for en gitt aktørID og saktype
-//     *
-//     * Foreløbig er alder, uføre og gjenlevende støttet i her men tjenesten støtter alle typer
-//     */
-//    fun kallBestemSak(requestBody: BestemSakRequest): BestemSakResponse? {
-//        return hentPesysSaker.measure {
-//            return@measure try {
-//                logger.info("Kaller bestemSak i PESYS")
-//                val headers = HttpHeaders()
-//                headers.contentType = MediaType.APPLICATION_JSON
-//
-//                val response = bestemSakOidcRestTemplate.exchange(
-//                        "/",
-//                        HttpMethod.POST,
-//                        HttpEntity(requestBody.toJson(), headers),
-//                        String::class.java)
-//
-//                mapper.readValue(response.body, BestemSakResponse::class.java)
-//            } catch (ex: HttpStatusCodeException) {
-//                throw RuntimeException("En feil oppstod under kall til bestemSak i PESYS ex: ", ex)
-//            } catch (ex: Exception) {
-//                throw RuntimeException("En feil oppstod under kall til bestemSak i PESYS ex: ", ex)
-//            }
-//        }
-//    }
-//}
-//
-//data class BestemSakRequest(val aktoerId: String,
-//                            val ytelseType: SakType,
-//                            val callId: UUID,
-//                            val consumerId: UUID)
-//
-//class BestemSakResponse(val feil: BestemSakFeil? = null,
-//                        val sakInformasjonListe: List<SakInformasjon>)
-//
-//class BestemSakFeil(val feilKode: String, val feilmelding: String)
-//
+@Component
+class BestemSakKlient(private val bestemSakOidcRestTemplate: RestTemplate,
+                      @Autowired(required = false) private val metricsHelper: MetricsHelper = MetricsHelper.ForTest()) {
+
+    private val logger: Logger by lazy { LoggerFactory.getLogger(BestemSakKlient::class.java) }
+    private val mapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+
+    private lateinit var hentPesysSaker: MetricsHelper.Metric
+
+    init {
+        hentPesysSaker = metricsHelper.init("hentPesysSaker")
+    }
+
+    /**
+     * Henter pesys sakID for en gitt aktørID og saktype
+     *
+     * Foreløbig er alder, uføre og gjenlevende støttet i her men tjenesten støtter alle typer
+     */
+    fun kallBestemSak(requestBody: BestemSakRequest): BestemSakResponse? {
+        return hentPesysSaker.measure {
+            return@measure try {
+                logger.info("Kaller bestemSak i PESYS")
+                val headers = HttpHeaders()
+                headers.contentType = MediaType.APPLICATION_JSON
+
+                val response = bestemSakOidcRestTemplate.exchange(
+                        "/",
+                        HttpMethod.POST,
+                        HttpEntity(requestBody.toJson(), headers),
+                        String::class.java)
+
+                mapper.readValue(response.body, BestemSakResponse::class.java)
+            } catch (ex: HttpStatusCodeException) {
+                throw RuntimeException("En feil oppstod under kall til bestemSak i PESYS ex: ", ex)
+            } catch (ex: Exception) {
+                throw RuntimeException("En feil oppstod under kall til bestemSak i PESYS ex: ", ex)
+            }
+        }
+    }
+}
+
+data class BestemSakRequest(val aktoerId: String,
+                            val ytelseType: SakType,
+                            val callId: UUID,
+                            val consumerId: UUID)
+
+class BestemSakResponse(val feil: BestemSakFeil? = null,
+                        val sakInformasjonListe: List<SakInformasjon>)
+
+class BestemSakFeil(val feilKode: String, val feilmelding: String)
+
