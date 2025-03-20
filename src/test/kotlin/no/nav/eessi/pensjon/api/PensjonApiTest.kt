cd28ea11f284eb22cfce534fe6bc417983c0d7a9
Daniel Skarpås <dskarpas@gmail.com>
2025-03-20 12:05:34 +0100

    t - disabler test som ikke benyttes

--- src/test/kotlin/no/nav/eessi/pensjon/api/PensjonApiTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/api/PensjonApiTest.kt
@@ -13,111 +13,111 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 
-class PensjonApiTest {
-
-
-    private val pensjonsinformasjonClient: PensjonsinformasjonClient = mockk()
-    private val behandleHendelseKlient: BehandleHendelseKlient = mockk()
-    private val bestemSakKlient: BestemSakKlient = mockk()
-
-    private val fagmodulRestMock: RestTemplate = mockk()
-    private val fagmodulKlient = FagmodulKlient(fagmodulRestMock)
-
-    private val api = PensjonApi(pensjonsinformasjonClient, behandleHendelseKlient, bestemSakKlient, fagmodulKlient)
-
-    @Test
-    fun hentAltPaaAktoer() {
-        val req = "<!xml>sasdad"
-        val aktoer = "1231231231"
-        val xmlString = javaClass.getResource("/pensjon/Vedtak-AlderUtland.xml")!!.readText()
-
-        @Suppress("DEPRECATION")
-        every { pensjonsinformasjonClient.hentAltPaaAktoerId(any(), any()) } returns xmlString
-
-        val result = api.hentSaker(req, aktoer)
-
-        @Suppress("DEPRECATION")
-        verify (exactly = 1) { pensjonsinformasjonClient.hentAltPaaAktoerId(any(), any()) }
-        assertEquals(xmlString, result)
-
-    }
-
-
-    @Test
-    fun hentAltPaaFnr() {
-        val req = "<!xml>sasdad"
-        val fnr = "1231231231"
-        val xmlString = javaClass.getResource("/pensjon/Vedtak-AlderUtland.xml")!!.readText()
-
-        every { pensjonsinformasjonClient.hentAltPaaFnr(any(), any()) } returns xmlString
-
-        val result = api.hentSakerPaaFnr(req, fnr)
-        verify (exactly = 1) { pensjonsinformasjonClient.hentAltPaaFnr(any(), any()) }
-        assertEquals(xmlString, result)
-
-    }
-
-    @Test
-    fun hentAltVedtak() {
-        val req = "<!xml>sasdad"
-        val vedtak = "1231231231"
-        val xmlString = javaClass.getResource("/pensjon/Vedtak-AlderUtland.xml")!!.readText()
-
-        every { pensjonsinformasjonClient.hentAltPaaVedtak(any(), any()) } returns xmlString
-
-        val result = api.hentVedtak (req, vedtak)
-        verify (exactly = 1) { pensjonsinformasjonClient.hentAltPaaVedtak(any(), any()) }
-        assertEquals(xmlString, result)
-
-    }
-
-    @Test
-    fun hentBestamSak() {
-        val bestemReq = mockk<BestemSakRequest>(relaxed = true)
-        val mockResponse = BestemSakResponse(sakInformasjonListe = listOf(mockk<SakInformasjon>(relaxed = true)))
-
-        every { bestemSakKlient.kallBestemSak(any()) } returns mockResponse
-
-        val result = bestemSakKlient.kallBestemSak(bestemReq)
-
-        verify (exactly = 1) { bestemSakKlient.kallBestemSak(bestemReq) }
-        assertEquals(mockResponse, result)
-
-    }
-
-    @Test
-    fun hendelse() {
-
-        justRun {  behandleHendelseKlient.opprettBehandleHendelse(any()) }
-
-        api.behandleHendelse("bla bla bla bla")
-
-        verify (exactly = 1) { behandleHendelseKlient.opprettBehandleHendelse(any()) }
-
-    }
-
-
-    @Test
-    fun fagmodulKrav() {
-        val bucId = "324234"
-        val mockResponse = "mock Json"
-
-        every { fagmodulRestMock.exchange(
-                    any<String>(),
-                    HttpMethod.GET,
-                    null,
-                    String::class.java)
-        } returns ResponseEntity( mockResponse, HttpStatus.OK)
-
-        val result = api.hentKravUtland(bucId)
-
-        verify (exactly = 1) { fagmodulRestMock.exchange(
-            "/pesys/hentKravUtland/$bucId",
-            HttpMethod.GET,
-            null,
-            String::class.java) }
-        assertEquals(mockResponse, result)
-    }
-
-
-}
+//class PensjonApiTest {
+//
+//
+//    private val pensjonsinformasjonClient: PensjonsinformasjonClient = mockk()
+//    private val behandleHendelseKlient: BehandleHendelseKlient = mockk()
+//    private val bestemSakKlient: BestemSakKlient = mockk()
+//
+//    private val fagmodulRestMock: RestTemplate = mockk()
+//    private val fagmodulKlient = FagmodulKlient(fagmodulRestMock)
+//
+//    private val api = PensjonApi(pensjonsinformasjonClient, behandleHendelseKlient, bestemSakKlient, fagmodulKlient)
+//
+//    @Test
+//    fun hentAltPaaAktoer() {
+//        val req = "<!xml>sasdad"
+//        val aktoer = "1231231231"
+//        val xmlString = javaClass.getResource("/pensjon/Vedtak-AlderUtland.xml")!!.readText()
+//
+//        @Suppress("DEPRECATION")
+//        every { pensjonsinformasjonClient.hentAltPaaAktoerId(any(), any()) } returns xmlString
+//
+//        val result = api.hentSaker(req, aktoer)
+//
+//        @Suppress("DEPRECATION")
+//        verify (exactly = 1) { pensjonsinformasjonClient.hentAltPaaAktoerId(any(), any()) }
+//        assertEquals(xmlString, result)
+//
+//    }
+//
+//
+//    @Test
+//    fun hentAltPaaFnr() {
+//        val req = "<!xml>sasdad"
+//        val fnr = "1231231231"
+//        val xmlString = javaClass.getResource("/pensjon/Vedtak-AlderUtland.xml")!!.readText()
+//
+//        every { pensjonsinformasjonClient.hentAltPaaFnr(any(), any()) } returns xmlString
+//
+//        val result = api.hentSakerPaaFnr(req, fnr)
+//        verify (exactly = 1) { pensjonsinformasjonClient.hentAltPaaFnr(any(), any()) }
+//        assertEquals(xmlString, result)
+//
+//    }
+//
+//    @Test
+//    fun hentAltVedtak() {
+//        val req = "<!xml>sasdad"
+//        val vedtak = "1231231231"
+//        val xmlString = javaClass.getResource("/pensjon/Vedtak-AlderUtland.xml")!!.readText()
+//
+//        every { pensjonsinformasjonClient.hentAltPaaVedtak(any(), any()) } returns xmlString
+//
+//        val result = api.hentVedtak (req, vedtak)
+//        verify (exactly = 1) { pensjonsinformasjonClient.hentAltPaaVedtak(any(), any()) }
+//        assertEquals(xmlString, result)
+//
+//    }
+//
+//    @Test
+//    fun hentBestamSak() {
+//        val bestemReq = mockk<BestemSakRequest>(relaxed = true)
+//        val mockResponse = BestemSakResponse(sakInformasjonListe = listOf(mockk<SakInformasjon>(relaxed = true)))
+//
+//        every { bestemSakKlient.kallBestemSak(any()) } returns mockResponse
+//
+//        val result = bestemSakKlient.kallBestemSak(bestemReq)
+//
+//        verify (exactly = 1) { bestemSakKlient.kallBestemSak(bestemReq) }
+//        assertEquals(mockResponse, result)
+//
+//    }
+//
+//    @Test
+//    fun hendelse() {
+//
+//        justRun {  behandleHendelseKlient.opprettBehandleHendelse(any()) }
+//
+//        api.behandleHendelse("bla bla bla bla")
+//
+//        verify (exactly = 1) { behandleHendelseKlient.opprettBehandleHendelse(any()) }
+//
+//    }
+//
+//
+//    @Test
+//    fun fagmodulKrav() {
+//        val bucId = "324234"
+//        val mockResponse = "mock Json"
+//
+//        every { fagmodulRestMock.exchange(
+//                    any<String>(),
+//                    HttpMethod.GET,
+//                    null,
+//                    String::class.java)
+//        } returns ResponseEntity( mockResponse, HttpStatus.OK)
+//
+//        val result = api.hentKravUtland(bucId)
+//
+//        verify (exactly = 1) { fagmodulRestMock.exchange(
+//            "/pesys/hentKravUtland/$bucId",
+//            HttpMethod.GET,
+//            null,
+//            String::class.java) }
+//        assertEquals(mockResponse, result)
+//    }
+//
+//
+//}
