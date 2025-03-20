e1a55e9a796606495492ebc4cb4ea03947a48935
Daniel Skarpås <dskarpas@gmail.com>
2025-03-20 12:02:35 +0100

    r - faser ut alt av pensjon og andre API

--- src/main/kotlin/no/nav/eessi/pensjon/klienter/FagmodulKlient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/klienter/FagmodulKlient.kt
@@ -5,24 +5,24 @@ import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
-@Component
-class FagmodulKlient(private val fagmodulOAuth2RestTemplate: RestTemplate) {
-
-    private val logger = LoggerFactory.getLogger(FagmodulKlient::class.java)
-
-    fun hentJsonDataFraFagmodul(requestPath: String): String {
-        return try {
-            logger.info("Kaller fagmodul med: $requestPath")
-            val responseEntity = fagmodulOAuth2RestTemplate.exchange(
-                requestPath,
-                HttpMethod.GET,
-                null,
-                String::class.java
-            )
-            responseEntity.body!!
-        } catch (ex: Exception) {
-            throw ex
-        }
-    }
-}
-
+//@Component
+//class FagmodulKlient(private val fagmodulOAuth2RestTemplate: RestTemplate) {
+//
+//    private val logger = LoggerFactory.getLogger(FagmodulKlient::class.java)
+//
+//    fun hentJsonDataFraFagmodul(requestPath: String): String {
+//        return try {
+//            logger.info("Kaller fagmodul med: $requestPath")
+//            val responseEntity = fagmodulOAuth2RestTemplate.exchange(
+//                requestPath,
+//                HttpMethod.GET,
+//                null,
+//                String::class.java
+//            )
+//            responseEntity.body!!
+//        } catch (ex: Exception) {
+//            throw ex
+//        }
+//    }
+//}
+//
